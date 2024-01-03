a6987140aff28c7d2fbc641cbeb9b0ac9b50909e
shanag1801 <shanag0110@gmail.com>
2024-01-03 08:48:36 +0200

    On click function

--- app/src/main/java/com/example/shaharapp3/MainActivity.java
+++ app/src/main/java/com/example/shaharapp3/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.shaharapp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        count = 0;
+    }
+    @SuppressLint("SetTextI18n")
+    public void go(View view){
+        count++;
+        if(count == 6)
+            btn.setText("Enough to click. Go to new start!");
     }
 }
